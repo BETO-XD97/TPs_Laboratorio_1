9f28576f5a4ba25aab3774587eff80c9d87dab5c
roberto <cd>
2022-06-17 16:13:25 -0300

    Actualizacion 1.0

---
+++ Segundo_Parcial/src/Segundo_Parcial.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : Segundo_Parcial.c
+ Author      : Roberto Rocabado
+ Division    : D
+ Description : Segundo_Parcial
+ DNI         : 40731247
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+int main(void) {
+
+	return 0;
+}
